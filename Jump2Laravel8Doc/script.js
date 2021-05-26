35c45a38636476b7eab15c37fd8c9fc20e6d153b
lunain84 <h_takeshita@yumemi.co.jp>
2021-05-26 16:12:25 +0900

    first commit

---
+++ Jump2Laravel8Doc/script.js
@@ -0,0 +1,21 @@
+//現在のURLを取得
+let currentPageUrl = location.href;
+//正規表現でドキュメントのバージョンを取得
+let versionStr = currentPageUrl.match(/\/[0-9]\.([0-9]|x)\//);
+//遷移先URLの初期化
+let newUrl = '';
+if (versionStr !== null && versionStr[0] !== '/8.x/') {
+    //8.xのURLに書き換え
+    newUrl = currentPageUrl.replace(versionStr[0], '/8.x/');
+
+    //遷移先にページがあるかどうか
+    fetch(newUrl)
+    .then(response => {
+        if (response.status !== 200) {
+            return;
+        }
+    });
+
+    //遷移前の履歴を残さずに遷移
+    location.replace(newUrl);
+}
