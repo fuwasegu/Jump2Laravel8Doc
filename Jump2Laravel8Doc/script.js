18f4bc8aa1b5ea5680beaa0d76fb393ad403fa18
lunain84 <h_takeshita@yumemi.co.jp>
2021-05-27 14:01:17 +0900

    200からokにした

--- Jump2Laravel8Doc/script.js
+++ Jump2Laravel8Doc/script.js
@@ -1,21 +1,21 @@
 //現在のURLを取得
 let currentPageUrl = location.href;
 //正規表現でドキュメントのバージョンを取得
-let oldVersionStr = currentPageUrl.match(/\/[0-9]\.([0-9]|x)\//);
+let oldVersionStr = currentPageUrl.match(/\/[0-9]\.([0-9]|x|dev)\//);
 //遷移先URLの初期化
 let newUrl = '';
-if (versionStr !== null && oldVersionStr[0] !== '/8.x/') {
+if (oldVersionStr !== null && oldVersionStr[0] !== '/8.x/') {
     //8.xのURLに書き換え
     newUrl = currentPageUrl.replace(oldVersionStr[0], '/8.x/');
 
     //遷移先にページがあるかどうか
     fetch(newUrl)
     .then(response => {
-        if (response.status === 200) {
-            //遷移前の履歴を残さずに遷移
-            location.replace(newUrl);
+        if (response.ok) {
+             //遷移前の履歴を残さずに遷移
+             location.replace(newUrl);
         }else{
-            alert('8.x系のドキュメントページは存在しないようです.このページに止まります．')
+            alert('8.x系のドキュメントページは存在しないようです.このページにとどまります．');
         }
     });
 }
