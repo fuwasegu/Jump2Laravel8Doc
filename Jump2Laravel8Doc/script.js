d3839fb63128cf56454f4d9a0ae04708acc57ee4
Yuichiro Luke Smith <contact@yu-smith.com>
2021-06-02 19:54:30 +0900

    add comments

--- Jump2Laravel8Doc/script.js
+++ Jump2Laravel8Doc/script.js
@@ -2,10 +2,13 @@
 const latestVersion = '8.x';
 //バージョン部分を表すRegExp
 const versionRegex = /[0-9]\.([0-9]|x|dev)/
-//現在のURLを配列として取得
+//現在のURLを取得
 const currentPageUrl = location.href;
+//スラッシュで区切られたパーツごとの配列に分解
 const currentPageUrlArray = currentPageUrl.split('/');
+//バージョンの要素を最新のものに置き換え
 const replacedUrl = currentPageUrlArray.map(part => part.match(versionRegex) ? latestVersion : part).join('/');
+//現在のURLとマッチするかを確認
 if (replacedUrl !== currentPageUrl) {
   //遷移先にページがあるかどうかをHEADリクエストで確認
   fetch(replacedUrl, { method: "HEAD" })
